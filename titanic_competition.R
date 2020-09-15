babe2b1e295e7c8c4f4c33ca10d02e61eef9e836
mganopolsky <ganopolsky.marina@gmail.com>
2020-09-15 15:02:29 -0400

    first random_forest_submission

---
+++ titanic_competition.R
@@ -0,0 +1,89 @@
+# This R environment comes with many helpful analytics packages installed
+# It is defined by the kaggle/rstats Docker image: https://github.com/kaggle/docker-rstats
+# For example, here's a helpful package to load
+
+library(tidyverse) # metapackage of all tidyverse packages
+library(randomForest)
+library(caret)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+dir_path <- "../input/titanic/"
+#list.files(path = dir_path)
+
+# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+train_set <- read.csv(paste(dir_path, "train.csv", sep=""))
+#head(train_set)
+test_set <- read.csv(paste(dir_path, "test.csv", sep=""))
+#head(test_set)
+
+train_set <- train_set %>% mutate_at( vars(Sex), as.factor) %>% mutate_at(vars(Survived), as.factor)
+test_set <- test_set %>% mutate_at( vars(Sex), as.factor) 
+
+
+women <- train_set %>% filter(Sex == "female")
+men <- train_set %>% filter(Sex == "male")
+
+percent_women_survived <- women %>% filter(Survived == 1) %>% summarize(n()) / nrow(women)
+percent_men_survived <- men %>% filter(Survived == 1) %>% summarize(n()) / nrow(men)
+percent_women_survived
+percent_men_survived
+
+#no missing passengerIds
+length(unique(train_set$PassengerId)) == nrow(train_set)
+
+train_set_count <- nrow(train_set)
+
+#are there missing ages?
+avg_age_train <- train_set %>% group_by(Pclass, Sex) %>% summarize(avg_age = mean(Age,  na.rm = TRUE))  %>% as.data.frame()
+avg_age_test <- test_set %>% group_by(Pclass, Sex) %>% summarize(avg_age = mean(Age,  na.rm = TRUE))  %>% as.data.frame()
+
+avg_fare_pclass_train <- train_set %>% group_by(Pclass) %>% summarize(avg_fare = mean(Fare,  na.rm = TRUE)) %>% as.data.frame()
+avg_fare_pclass_test <- test_set %>% group_by(Pclass) %>% summarize(avg_fare = mean(Fare,  na.rm = TRUE))  %>% as.data.frame()
+
+merge_cols <- c("Pclass", "Sex")
+
+train_set <- inner_join(train_set, avg_age_train, by = merge_cols)
+test_set <- inner_join(test_set, avg_age_test, by = merge_cols)
+
+train_set <- inner_join(train_set, avg_fare_pclass_train, by = "Pclass")
+test_set <- inner_join(test_set, avg_fare_pclass_test, by = "Pclass")
+
+#update Age is it's NA based on AVG age for that PClass and Sex
+train_set <- train_set %>% mutate(Age = ifelse(is.na(Age) , avg_age, Age))
+test_set <- test_set %>% mutate(Age = ifelse(is.na(Age) , avg_age, Age))
+
+#update Fare is it's NA based on AVG age for that PClass and Sex
+train_set <- train_set %>% mutate(Fare = ifelse(Fare == 0 | is.na(Fare) , avg_fare, Fare))
+test_set <- test_set %>% mutate(Fare = ifelse(Fare == 0  | is.na(Fare), avg_fare, Fare))
+
+#add 'Family_Size'field as combo of sibblings and parent/child count
+train_set <- train_set %>% mutate(Family_Size = SibSp + Parch )
+test_set <- test_set %>% mutate(Family_Size = SibSp + Parch )
+
+
+#add 'Age*Class'field as product of Age * Class
+train_set <- train_set %>% mutate(Age_Class = Age + Pclass )
+test_set <- test_set %>% mutate(Age_Class = Age + Pclass )
+
+#try a random forest machine learning model 
+y <- train_set$Survived
+
+features <- c("Pclass", "Sex", "SibSp", "Parch")
+X <- train_set %>% select(features)
+
+set.seed(1)
+
+fit <- randomForest(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare +
+                      Family_Size,
+                    data=train_set, 
+                    importance=TRUE, 
+                    ntree=2000)
+
+#varImpPlot(fit)
+
+Prediction <- predict(fit, test_set)
+
+submit <- data.frame(PassengerId = test_set$PassengerId, Survived = Prediction)
